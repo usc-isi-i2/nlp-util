885c5e4d6cf6f92d66bd25e481c728d9a48634ff
Ryan Gabbard <gabbard@isi.edu>
2017-10-31 11:47:50 -0400

    Allow specifying colors by RGB

--- gnuplot-util/src/main/java/com/bbn/bue/gnuplot/Color.java
+++ gnuplot-util/src/main/java/com/bbn/bue/gnuplot/Color.java
@@ -3,6 +3,8 @@ package com.bbn.bue.gnuplot;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.collect.Range;
+
 public final class Color {
 
   private final String colorString;
@@ -21,11 +23,32 @@ public final class Color {
     return new Color(colorString);
   }
 
+  public static Color fromRGB255(int red, int green, int blue) {
+    return fromHexString("#" + rgbComponentToHex(red, "red")
+        + rgbComponentToHex(green, "green")
+        + rgbComponentToHex(blue, "blue"));
+  }
+
   public String asColorString() {
     return colorString;
   }
 
   public String asQuotedColorString() {
     return "\"" + asColorString() + "\"";
+  }
+
+  private static final Range<Integer> TWO_FIFTY_FIVE = Range.closed(0, 255);
+
+  private static String rgbComponentToHex(int colorComponent, String componentName) {
+    checkArgument(TWO_FIFTY_FIVE.contains(colorComponent),
+        "RGB color values must lie in [0,255] but got "
+            + componentName + " component %s", colorComponent);
+
+    if (colorComponent == 0) {
+      // need to special case to ensure two digits
+      return "00";
+    } else {
+      return Integer.toHexString(colorComponent).toUpperCase();
+    }
   }
 }
