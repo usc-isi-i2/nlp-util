1a0f0828f9a1a5792c93df407e72c5b8c645c85e
Ryan Gabbard <gabbard@isi.edu>
2017-10-31 11:48:18 -0400

    Add color-blind-friendly seven color gnuplot palette

--- gnuplot-util/src/main/java/com/bbn/bue/gnuplot/Palette.java
+++ gnuplot-util/src/main/java/com/bbn/bue/gnuplot/Palette.java
@@ -3,7 +3,6 @@ package com.bbn.bue.gnuplot;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
-
 import java.util.List;
 
 public final class Palette {
@@ -20,4 +19,33 @@ public final class Palette {
   public Iterable<Color> infinitePaletteLoop() {
     return Iterables.cycle(colorList);
   }
+
+  /**
+   * A seven-color palette safe for deuteranopia.
+   *
+   * Taken from B. Wong, "Points of view: Color blindness" Nature Methods 8:411 (2011) via
+   * http://mkweb.bcgsc.ca/colorblind/
+   */
+  public static Palette colorBlindSafe7() {
+    return COLORBLIND_SAFE_7;
+  }
+
+  private static final Palette COLORBLIND_SAFE_7 = Palette.from(ImmutableList.of(
+      // black
+      Color.fromRGB255(0, 0, 0),
+      // orange
+      Color.fromRGB255(230, 159, 0),
+      // sky blue
+      Color.fromRGB255(86, 180, 233),
+      // blueish green
+      Color.fromRGB255(0, 158, 115),
+      // yellow
+      Color.fromRGB255(240, 228, 66),
+      // blue
+      Color.fromRGB255(0, 114, 178),
+      // vermillion
+      Color.fromRGB255(213, 94, 0),
+      // reddish purple
+      Color.fromRGB255(204, 121, 167)
+  ));
 }
