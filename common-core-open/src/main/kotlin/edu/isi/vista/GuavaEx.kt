87789a573e7c749a5480def8058ddd7445c21a9c
Ryan Gabbard <gabbard@isi.edu>
2019-05-09 14:22:01 -0400

    Kotlin extension methods for Guava multimaps

--- common-core-open/src/main/kotlin/edu/isi/vista/GuavaEx.kt
+++ common-core-open/src/main/kotlin/edu/isi/vista/GuavaEx.kt
@@ -1,6 +1,7 @@
 package edu.isi.vista
 
 import com.google.common.collect.*
+import java.util.function.Function
 
 fun <T> Sequence<T>.toImmutableList(): ImmutableList<T> {
     return ImmutableList.copyOf(this.asIterable())
@@ -24,5 +25,45 @@ fun <K, V> Map<K, V>.toImmutableMap(): ImmutableMap<K, V> = ImmutableMap.copyOf(
 fun <K : Comparable<K>, V> Sequence<Pair<Range<K>,V>>.toImmutableRangeMap() : ImmutableRangeMap<K, V> {
     val ret = ImmutableRangeMap.builder<K, V>()
     this.forEach { (k, v) -> ret.put(k, v) }
+    return ret.build()
+}
+
+
+/**
+ * Returns an ImmutableListMultimap containing the elements from the given sequence indexed by the key returned from
+ * keySelector function applied to each element.
+ *
+ * Each item of the sequence will be appear as a value in the resulting multimap,
+ * yielding a multimap with the same size as the input sequence.
+ * The key used to store that value in the multimap will be the result of calling the function on that value.
+ *
+ * In the returned multimap, keys appear in the order they are first encountered, and the values corresponding to
+ * each key appear in the same order as they are encountered.
+ * The returned map preserves the entry iteration order of the original sequence.
+ *
+ * This is just a wrapper around Guava's Multimaps.index()
+ *
+ * The operation is terminal.
+ */
+fun <T, K> Sequence<T>.associateByToListMultimap(keySelector: (T) -> K) : ImmutableListMultimap<K, T> {
+    val ret = ImmutableListMultimap.builder<K, T>()
+    this.forEach { ret.put(keySelector(it), it) }
+    return ret.build()
+}
+
+/**
+ * Returns an ImmutableSetMultimap containing the elements from the given sequence indexed by the key returned from
+ * keySelector function applied to each element.
+ *
+ * Each item of the sequence will be appear as a value in the resulting multimap,
+ * yielding a multimap with the same size as the input sequence. The key used to store that value in the multimap will be the result of calling the function on that value. The resulting multimap is created as an immutable snapshot. In the returned multimap, keys appear in the order they are first encountered, and the values corresponding to each key appear in the same order as they are encountered.
+ *
+ * Order guarantees are pending https://github.com/google/guava/issues/3460
+ *
+ * The operation is terminal.
+ */
+fun <T, K> Sequence<T>.associateByToSetMultimap(keySelector: (T) -> K) : ImmutableSetMultimap<K, T> {
+    val ret = ImmutableSetMultimap.builder<K, T>()
+    this.forEach { ret.put(keySelector(it), it) }
     return ret.build()
 }
