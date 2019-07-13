1eb7ff7832f2d9723e212495034e784c44151c21
Ryan Gabbard <gabbard@isi.edu>
2019-07-13 18:31:23 -0400

    Program to export from GraphDB via a local repository connection

---
+++ nlp-core-open/src/main/java/edu/isi/nlp/ExportGraphDB.java
@@ -0,0 +1,31 @@
+package edu.isi.nlp;
+
+import edu.isi.nlp.parameters.Parameters;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.manager.LocalRepositoryManager;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFWriter;
+import org.eclipse.rdf4j.rio.Rio;
+
+// this doesn't belong here, but I needed a spot to stick an emergency evaluation hack
+public final class ExportGraphDB {
+  public static void main(String[] argv) throws IOException {
+    final Parameters params = Parameters.loadSerifStyle(new File(argv[0]));
+    final File outputFile = params.getCreatableFile("outputFile");
+    final LocalRepositoryManager localRepositoryManager =
+        new LocalRepositoryManager(params.getExistingDirectory("graphDbBaseDir"));
+    final Repository repository =
+        localRepositoryManager.getRepository(params.getString("repositoryId"));
+
+    try (RepositoryConnection connection = repository.getConnection()) {
+      try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+        final RDFWriter writer = Rio.createWriter(RDFFormat.NTRIPLES, outputStream);
+        connection.exportStatements(null, null, null, false, writer);
+      }
+    }
+  }
+}
